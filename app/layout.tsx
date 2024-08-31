81633039d1e75d4d10b904e9532d41d181b75c96
baloerrr <akbaradityah444@gmail.com>
2024-09-01 03:46:34 +0700

    fix: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,58 +9,9 @@ const pressStart = Press_Start_2P({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Valter 2024",
-  description:
-    "Welcome to the personal portfolio of Muhammad Ariq Farhan, a versatile software engineer, colorist, and RPA specialist with expertise in web and mobile app development, automation, and video editing.",
-  keywords: [
-    "Muhammad Ariq Farhan",
-    "software engineer",
-    "colorist",
-    "RPA specialist",
-    "web development",
-    "mobile app development",
-    "robotic process automation",
-    "video editing",
-    "portfolio",
-  ],
-  authors: [{ name: "Muhammad Ariq Farhan" }],
-  creator: "Muhammad Ariq Farhan",
-  publisher: "Muhammad Ariq Farhan",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
-  openGraph: {
-    title: "Muhammad Ariq Farhan",
-    description:
-      "Personal portfolio showcasing expertise in software engineering, coloring, and RPA.",
-    url: "https://www.yourwebsite.com",
-    siteName: "Muhammad Ariq Farhan Portfolio",
-    images: [
-      {
-        url: "https://www.yourwebsite.com/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Muhammad Ariq Farhan Portfolio",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Muhammad Ariq Farhan | Software Engineer & Colorist",
-    description:
-      "Personal portfolio showcasing expertise in software engineering, coloring, and RPA.",
-    creator: "@ariqfrhan",
-    images: ["https://www.yourwebsite.com/twitter-image.jpg"],
-  },
-  robots: "index, follow",
 };
 
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1.0,
 };
 
 export default function RootLayout({
